795393d8c5b50e8fb79fca8d330a9cfbc5c8c1cf
Hunter Hastings <hunter.hastings@gmail.com>
2012-02-19 18:05:54 -0600

    fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hnpro::Application.config.secret_token = '6b9343c5a46489157f9b249c46d499b012f0dd186211dc26e600c9b43a2e784b8c7dda12d13d35ea73c571ea6fa350c5a17d1df8d2ed0cb8077da28fc1b40e84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hnpro::Application.config.session_store :cookie_store, :key => '_hnpro_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hnpro::Application.config.session_store :active_record_store
